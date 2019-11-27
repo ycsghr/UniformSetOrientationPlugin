de8991207b377309ec9f126bbccd356a3c38f584
yangchao <yangchao@muztg.com>
2019-11-27 14:44:24 +0800

    fix

--- buildSrc/src/main/groovy/com/yc/buildsrc/OrientationPlugin.groovy
+++ buildSrc/src/main/groovy/com/yc/buildsrc/OrientationPlugin.groovy
@@ -1,10 +1,7 @@
-import org.codehaus.groovy.reflection.android.AndroidSupport
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-import javax.xml.namespace.QName
-
-class OrientationPlugin implements Plugin<Project>{
+class OrientationPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
@@ -12,57 +9,60 @@ class OrientationPlugin implements Plugin<Project>{
         def extension = project.extensions.create('testSet', TestsetOrientationPlugin)
 
         project.afterEvaluate {
-            project.tasks.getByName("processDebugManifest") {
+            project.plugins.withId('com.android.application') {
 
-                project.android.applicationVariants.all { variant ->
-                    variant.outputs.each { output ->
-                        output.getProcessManifestProvider().get().doLast {
-                            //合并时Mainfest地址
-                            String manifestPathDirectory =manifestOutputDirectory.get()
-                            String manifestPath=manifestPathDirectory+'/AndroidManifest.xml'
-                            println('manifestPath='+manifestPath)
-                            String  manifestContent = new File(manifestPath).getText('UTF-8')
-                            println('manifestContent='+manifestContent)
-                            def xml = new XmlParser().parseText(manifestContent)
-                            println('xml='+xml)
-                            def activitys = xml.application[0].activity
-                            println('activitys='+activitys)
+                project.tasks.getByName("processDebugManifest") {
 
-                            activitys.each { activity ->
-                                println('activity='+activity)
-                                def attrs = activity.attributes()
-                                def isAddScreenOrientation = true
-                                attrs.each { attr ->
-                                    def key= attr.getKey()
-                                    if ('name'.equals(key.localPart)&&extension.dontSetActivityNames.contains(attr.getValue())){
-                                        isAddScreenOrientation=false
+                    project.android.applicationVariants.all { variant ->
+                        variant.outputs.each { output ->
+                            output.getProcessManifestProvider().get().doLast {
+                                //合并时Mainfest地址
+                                String manifestPathDirectory = manifestOutputDirectory.get()
+                                String manifestPath = manifestPathDirectory + '/AndroidManifest.xml'
+                                println('manifestPath=' + manifestPath)
+                                String manifestContent = new File(manifestPath).getText('UTF-8')
+                                println('manifestContent=' + manifestContent)
+                                def xml = new XmlParser().parseText(manifestContent)
+                                println('xml=' + xml)
+                                def activitys = xml.application[0].activity
+                                println('activitys=' + activitys)
 
-                                    }else if ('screenOrientation'.equals(key.localPart)){
-                                        isAddScreenOrientation=false
+                                activitys.each { activity ->
+                                    println('activity=' + activity)
+                                    def attrs = activity.attributes()
+                                    def isAddScreenOrientation = true
+                                    attrs.each { attr ->
+                                        def key = attr.getKey()
+                                        if ('name'.equals(key.localPart) && extension.dontSetActivityNames.contains(attr.getValue())) {
+                                            isAddScreenOrientation = false
+
+                                        } else if ('screenOrientation'.equals(key.localPart)) {
+                                            isAddScreenOrientation = false
+                                        }
+
+
+                                    }
+                                    println('isAddScreenOrientation=' + isAddScreenOrientation)
+                                    if (isAddScreenOrientation) {
+                                        attrs.put('android:screenOrientation', extension.screenOrientation)
                                     }
 
+                                }
+                                def serialize = groovy.xml.XmlUtil.serialize(xml)
+                                new File(manifestPath).write(serialize)
 
-                                }
-                                println('isAddScreenOrientation='+isAddScreenOrientation)
-                                if (isAddScreenOrientation){
-                                    attrs.put('android:screenOrientation', extension.screenOrientation)
-                                }
 
                             }
-                            def serialize = groovy.xml.XmlUtil.serialize(xml)
-                            new File(manifestPath).write(serialize)
 
 
                         }
-
 
                     }
 
                 }
 
+
             }
-
-
         }
     }
 
